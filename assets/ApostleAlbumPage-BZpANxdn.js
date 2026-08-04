380acb6feadc1b9b15163686f9e5bf2215993632
Telakour <cszh2403836456@gmail.com>
2026-08-04 12:11:34 +0800

    Fix mobile Studio motion selection

--- assets/ApostleAlbumPage-BZpANxdn.js
+++ assets/ApostleAlbumPage-BZpANxdn.js
@@ -1,4 +1,4 @@
-import{f as e}from"./index-BqW73FG8.js";/**
+import{f as e}from"./index-DBoFWASD.js";/**
  * @license lucide-react v1.16.0 - ISC
  *
  * This source code is licensed under the ISC license.
