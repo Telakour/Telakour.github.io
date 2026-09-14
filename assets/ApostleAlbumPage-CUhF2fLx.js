ffb1e8159315013fc67d1abb2959d403b8061940
Telakour <Telakour@users.noreply.github.com>
2026-09-14 09:07:02 +0800

    Fix English header title framing

--- assets/ApostleAlbumPage-CUhF2fLx.js
+++ assets/ApostleAlbumPage-CUhF2fLx.js
@@ -1,4 +1,4 @@
-import{f as e}from"./index-BYSJIxBO.js";/**
+import{f as e}from"./index-CvQU12-T.js";/**
  * @license lucide-react v1.16.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/studioCreatorHandoff-COH5keS7.js
+++ assets/studioCreatorHandoff-COH5keS7.js
@@ -1,4 +1,4 @@
-import{f as x}from"./index-BYSJIxBO.js";import{r as le,o as ue}from"./SpineResourceCatalog-MuczjGHM.js";import{S as G,r as fe}from"./BundledSpineResources-BfZGuG3E.js";/**
+import{f as x}from"./index-CvQU12-T.js";import{r as le,o as ue}from"./SpineResourceCatalog-MuczjGHM.js";import{S as G,r as fe}from"./BundledSpineResources-BfZGuG3E.js";/**
  * @license lucide-react v1.16.0 - ISC
  *
  * This source code is licensed under the ISC license.
